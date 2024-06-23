00ee4beb66dec19f584299d8c9e43aa0c774c1a7
Sudeep Adhikari <sudeep2p@gmail.com>
2024-06-23 15:35:24 +0545

    single-user.html complete

--- users/views.py
+++ users/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Profile
+
 
 # Create your views here.
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = get_object_or_404(Profile, id=pk)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
+    return render(request, 'users/user-profile.html', context)
